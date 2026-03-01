af3a709c771c7d5188d02142c53bba6d755785e2
tomzit <barisakib6@gmail.com>
2026-03-01 16:56:00 +0600

    add firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:ui';
 import 'package:finalcrafty/app/crafty_bay.dart';
+import 'package:finalcrafty/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const CraftyBay());
 }
 
