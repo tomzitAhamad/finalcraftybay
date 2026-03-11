d35e442cf86498e4cc82d951afe91e05835be7e3
tomzit <barisakib6@gmail.com>
2026-03-11 23:54:02 +0600

    added splash screen

--- lib/features/auth/presentations/screens/splash_screen.dart
+++ lib/features/auth/presentations/screens/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
+
     super.initState();
     _navigateToNextScreen();
   }
