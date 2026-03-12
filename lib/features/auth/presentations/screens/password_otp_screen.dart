e72ad456eb4ad999fae292d1fb2a81ab65b995bc
tomzit <barisakib6@gmail.com>
2026-03-12 13:42:05 +0600

    added otp sectioin screen

--- lib/features/auth/presentations/screens/password_otp_screen.dart
+++ lib/features/auth/presentations/screens/password_otp_screen.dart
@@ -1,8 +1,10 @@
-import 'dart:async';
 import 'package:finalcrafty/app/app_colors.dart';
+import 'package:finalcrafty/features/auth/presentations/widgets/resend_otp_section.dart';
 import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
+import 'package:provider/provider.dart';
 import '../../../../app/extension/utils_extension.dart';
+import '../providers/otp_timer_provider.dart';
 import '../widgets/app_logo.dart';
 
 class PasswordOtpScreen extends StatefulWidget {
@@ -17,36 +19,13 @@ class _PasswordOtpScreenState extends State<PasswordOtpScreen> {
 
   final TextEditingController _otpController = TextEditingController();
 
-  int _secondsRemaining = 120;
-  Timer? _timer;
-
   @override
   void initState() {
     super.initState();
-    _startTimer();
-  }
 
-  /// START TIMER
-  void _startTimer() {
-    _secondsRemaining = 120;
-
-    _timer?.cancel();
-
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_secondsRemaining == 0) {
-        timer.cancel();
-      } else {
-        setState(() {
-          _secondsRemaining--;
-        });
-      }
+    Future.microtask(() {
+      context.read<OtpTimerProvider>().startTimer();
     });
-  }
-
-  /// RESEND OTP
-  void _onTapResendCode() {
-    _startTimer();
-    print("OTP Resent");
   }
 
   /// NEXT BUTTON
@@ -57,7 +36,6 @@ class _PasswordOtpScreenState extends State<PasswordOtpScreen> {
 
   @override
   void dispose() {
-    _timer?.cancel();
     _otpController.dispose();
     super.dispose();
   }
@@ -82,7 +60,9 @@ class _PasswordOtpScreenState extends State<PasswordOtpScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                SizedBox(height: 70,),
+
+                const SizedBox(height: 70),
+
                 const AppLogo(),
 
                 const SizedBox(height: 24),
@@ -107,47 +87,12 @@ class _PasswordOtpScreenState extends State<PasswordOtpScreen> {
                   controller: _otpController,
                   keyboardType: TextInputType.number,
                   defaultPinTheme: defaultPinTheme,
-                  onCompleted: (pin) {
-                    print('Entered OTP: $pin');
-                  },
                 ),
 
                 const SizedBox(height: 24),
 
-                /// TIMER TEXT
-                Text.rich(
-                  TextSpan(
-                    text: "This code will expire in ",
-                    style: context.textTheme.bodyLarge?.copyWith(
-                      color: Colors.grey,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "${_secondsRemaining}s",
-                        style: const TextStyle(
-                          color: AppColors.themeColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 8),
-
-                /// RESEND CODE
-                GestureDetector(
-                  onTap: _secondsRemaining == 0 ? _onTapResendCode : null,
-                  child: Text(
-                    'Resend Code',
-                    style: context.textTheme.bodyLarge?.copyWith(
-                      color: _secondsRemaining == 0
-                          ? AppColors.themeColor
-                          : Colors.grey,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
+                /// TIMER + RESEND (Consumer)
+                const ResendOtpSection(),
 
                 const SizedBox(height: 24),
 
