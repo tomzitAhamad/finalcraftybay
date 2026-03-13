61caec51fdd83dbf7811dc69e6f8a3b4eafeea13
tomzit <barisakib6@gmail.com>
2026-03-13 16:35:23 +0600

    added home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,9 +1,14 @@
 
 import 'package:finalcrafty/app/asset_paths.dart';
+import 'package:finalcrafty/features/home/presentation/widgets/home_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../shared/widgets/category_card.dart';
 import '../widgets/appBarIconButton.dart';
+import '../widgets/home_category_list.dart';
+import '../widgets/product_search_bar.dart';
+import '../widgets/section_header.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,8 +22,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+
+          children: [
+            SizedBox(height: 8,),
+            ProductSearchBar(),
+            SizedBox(height: 16,),
+            HomeSlider(),
+            SizedBox(height: 16,),
+            SectionHeader(name: 'Categories', onTapSeeAll: () {  },),
+            _buildCategoryList()
+          ],
+        ),
+      ),
     );
+
+
   }
+
+  Widget _buildCategoryList(){
+    return HomeCategoryList();
+  }
+
 
   AppBar _buildAppBar() {
     return AppBar(
@@ -37,4 +64,12 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+
 
