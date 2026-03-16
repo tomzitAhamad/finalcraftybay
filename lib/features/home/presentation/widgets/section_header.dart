f2c22cd4b13c10907f1a8c4982e41b09c24f5db0
tomzit <barisakib6@gmail.com>
2026-03-16 13:14:11 +0600

    added section header

--- lib/features/home/presentation/widgets/section_header.dart
+++ lib/features/home/presentation/widgets/section_header.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 import '../../../../app/extension/utils_extension.dart';
@@ -12,12 +12,15 @@ class SectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: .spaceBetween,
-      children: [
-        Text(name,style: context.textTheme.titleMedium,),
-        TextButton(onPressed: onTapSeeAll, child: Text('See all'))
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: .spaceBetween,
+        children: [
+          Text(name,style: context.textTheme.titleLarge,),
+          TextButton(onPressed: onTapSeeAll, child: Text('See all'))
+        ],
+      ),
     );
   }
 }
