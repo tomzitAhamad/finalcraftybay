4c5ae06dcb6fad2c77a4db0e3f2b33f79733cfeb
tomzit <barisakib6@gmail.com>
2026-03-16 13:01:45 +0600

    added product list by category ui

---
+++ lib/features/products/presentation/screens/product_list_screen.dart
@@ -0,0 +1,31 @@
+
+import 'package:finalcrafty/features/shared/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key, required this.categoryName});
+  final String categoryName;
+
+  static const String name='product-list';
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.categoryName),
+      ),
+      body: GridView.builder(
+          itemCount: 30,
+
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 4,
+              crossAxisCount: 3), itemBuilder: (context,index){
+        return FittedBox(child: ProductCard());
+      }),
+    );
+  }
+}
--- lib/features/shared/widgets/category_card.dart
+++ lib/features/shared/widgets/category_card.dart
@@ -1,3 +1,4 @@
+import 'package:finalcrafty/features/products/presentation/screens/product_list_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,19 +11,24 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: .center,
-      children: [
-        Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              color: AppColors.themeColor.withAlpha(30),
-              borderRadius: .circular(8)
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(context, ProductListScreen.name,arguments: 'Electronics');
+      },
+      child: Column(
+        crossAxisAlignment: .center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: AppColors.themeColor.withAlpha(30),
+                borderRadius: .circular(8)
+            ),
+            child: Icon(Icons.computer,size: 36,color: AppColors.themeColor,),
           ),
-          child: Icon(Icons.computer,size: 36,color: AppColors.themeColor,),
-        ),
-        Text('Electronics',style: TextStyle(fontSize: 16,fontWeight: .w600,color: AppColors.themeColor),)
-      ],
+          Text('Electronics',style: TextStyle(fontSize: 16,fontWeight: .w600,color: AppColors.themeColor),)
+        ],
+      ),
     );
   }
 }
