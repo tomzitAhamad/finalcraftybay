61bd10cba1fa642e18b07fec01c6ad5b9a55e11a
tomzit <barisakib6@gmail.com>
2026-03-16 12:01:01 +0600

    added home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,14 @@
 
 import 'package:finalcrafty/app/asset_paths.dart';
 import 'package:finalcrafty/features/home/presentation/widgets/home_slider.dart';
+import 'package:finalcrafty/features/shared/presentation/providers/main_nav_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
-import '../../../shared/widgets/category_card.dart';
 import '../widgets/appBarIconButton.dart';
 import '../widgets/home_category_list.dart';
+import '../widgets/horizental_product_list_view.dart';
 import '../widgets/product_search_bar.dart';
 import '../widgets/section_header.dart';
 
@@ -22,30 +24,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-
-          children: [
-            SizedBox(height: 8,),
-            ProductSearchBar(),
-            SizedBox(height: 16,),
-            HomeSlider(),
-            SizedBox(height: 16,),
-            SectionHeader(name: 'Categories', onTapSeeAll: () {  },),
-            _buildCategoryList()
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+        
+            children: [
+              SizedBox(height: 8,),
+              ProductSearchBar(),
+              SizedBox(height: 16,),
+              HomeSlider(),
+              SizedBox(height: 16,),
+              SectionHeader(name: 'Categories', onTapSeeAll: context.read<MainNavProvider>().moveToCategory),
+              HomeCategoryList(),
+              SectionHeader(name: 'Popular', onTapSeeAll: () {  },),
+              HorizentalProductListView(),
+              SectionHeader(name: 'Special', onTapSeeAll: () {  },),
+              HorizentalProductListView(),
+              SectionHeader(name: 'New', onTapSeeAll: () {  },),
+              HorizentalProductListView(),
+        
+            ],
+          ),
         ),
       ),
     );
 
 
   }
-
-  Widget _buildCategoryList(){
-    return HomeCategoryList();
-  }
-
 
   AppBar _buildAppBar() {
     return AppBar(
@@ -64,6 +70,10 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
 
 
 
