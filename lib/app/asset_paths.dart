a72648eeb16195505a5f78b3013c1cd10018a826
tomzit <barisakib6@gmail.com>
2026-03-16 13:11:46 +0600

    asset path

--- lib/app/asset_paths.dart
+++ lib/app/asset_paths.dart
@@ -2,4 +2,5 @@ class AssetPaths {
   static const String _imagePath='assets/images';
   static const String logoSvg='$_imagePath/logo-2.svg';
   static const String navLogoSvg='$_imagePath/logo_nav.svg';
+  static const String dummyImagePng='$_imagePath/shoee.jpeg';
 }
