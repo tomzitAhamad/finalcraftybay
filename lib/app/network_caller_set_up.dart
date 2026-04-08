3377d3628fa6b48b0f228e6d836da885841ac9a1
tomzit <barisakib6@gmail.com>
2026-04-08 19:41:10 +0600

    updated authentication

--- lib/app/network_caller_set_up.dart
+++ lib/app/network_caller_set_up.dart
@@ -1,19 +1,34 @@
+import 'package:finalcrafty/app/crafty_bay.dart';
 import 'package:flutter/material.dart';
+
 import '../core/services/network_caller.dart';
 import '../features/auth/presentations/screens/sign_in_screen.dart';
-import 'crafty_bay.dart';
+import 'controllers/auth_controller.dart';
 
 
-NetworkCaller getNetworkCaller(){
-  return NetworkCaller(headers:{
-    // 'token':'token',
-    'content-type':'application/json'
-  }, onUnauthorize: () {
-    _moveToSignInScreen();
-  });
+NetworkCaller getNetworkCaller() {
+  return NetworkCaller(
+    headers: () {
+      Map<String, String> headers = {
+        'content-type': 'application/json',
+      };
+      if (AuthController.token != null) {
+        headers['token'] = '${AuthController.token}';
+      }
+      return headers;
+    },
+    onUnauthorize: () async {
+      await AuthController.clearUserData();
+      _moveToSignInScreen();
+    },
+  );
 }
 
-void _moveToSignInScreen(){
-  Navigator.pushNamed(CraftyBay.navigatorKey.currentContext!,
-      SignInScreen.name);
+
+
+void _moveToSignInScreen() {
+  Navigator.pushNamed(
+    CraftyBay.navigatorKey.currentContext!,
+    SignInScreen.name,
+  );
 }
--- lib/features/auth/presentations/providers/sign_in_provider.dart
+++ lib/features/auth/presentations/providers/sign_in_provider.dart
@@ -20,6 +20,7 @@ class SignInProvider extends ChangeNotifier {
     final NetworkResponse response = await getNetworkCaller().postRequest(
       Urls.signInUrl,
       body: {'email': email, 'password': password},
+      isFromLogin: true
     );
 
     if (response.isSuccess) {
