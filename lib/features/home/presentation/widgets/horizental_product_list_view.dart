c03e983ec27aacda71ada7306e8529ad5531fec0
tomzit <barisakib6@gmail.com>
2026-04-11 14:38:24 +0600

    added popular,new section api

--- lib/features/home/presentation/widgets/horizental_product_list_view.dart
+++ lib/features/home/presentation/widgets/horizental_product_list_view.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../catagory/presentation/provider/category_list_provider.dart';
 
+import '../../../shared/widgets/category_card.dart';
 
-class HorizontalProductListView extends StatelessWidget {
-  const HorizontalProductListView({super.key});
+class HorizentalProductListView extends StatelessWidget {
+  const HorizentalProductListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 195,
-      child: ListView.builder(
-        scrollDirection: .horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          // return ProductCard();
+      height: 110,
+      child: Consumer<CategoryListProvider>(
+        builder: (context, categoryListProvider, _) {
+          if (categoryListProvider.getInitialCategoryListInProgress) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.separated(
+            scrollDirection: .horizontal,
+            itemCount: getCategoryListLength(
+              categoryListProvider.categories.length,
+            ),
+            itemBuilder: (context, index) => CategoryCard(
+              categoryModel: categoryListProvider.categories[index],
+            ),
+            separatorBuilder: (_, __) => const SizedBox(width: 12),
+          );
         },
       ),
     );
+  }
+
+  int getCategoryListLength(int length) {
+    return length > 10 ? 10 : length;
   }
 }
